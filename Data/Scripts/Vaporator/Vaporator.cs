20462a2181d236e0702f9a99a3c745fb1bd5310e
AnarchCassius <anarchcassius@gmail.com>
2019-03-11 16:52:11 -0700

    Survival Update
    Fixed Ice not being pulled from Vaporator.
    Added PCU and description.
    Increased operational power and output.

--- Data/Scripts/Vaporator/Vaporator.cs
+++ Data/Scripts/Vaporator/Vaporator.cs
@@ -29,54 +29,70 @@ using VRageMath;
 
 namespace Slowpokefarm.Vaporator
 {
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_OxygenGenerator), true, "LargeBlockVaporator")]
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Refinery), true, "LargeBlockVaporator")]
     public class Vaporator : MyGameLogicComponent
     {
         // Builder is nessassary for GetObjectBuilder method as far as I know.
         private MyObjectBuilder_EntityBase builder;
-		private Sandbox.ModAPI.IMyOxygenGenerator m_vaporator;
+		private Sandbox.ModAPI.IMyRefinery m_vaporator;
 		private IMyCubeBlock m_parent;
         private float w_density;
+		private float poweruse;
 
         Sandbox.ModAPI.IMyTerminalBlock terminalBlock;
         Vape_AtmosphereDetector atmoDet = new Vape_AtmosphereDetector();
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
-			m_vaporator = Entity as Sandbox.ModAPI.IMyOxygenGenerator;
+			m_vaporator = Entity as Sandbox.ModAPI.IMyRefinery;
             m_parent = Entity as IMyCubeBlock;
 
-            m_vaporator.UpgradeValues.Add("Productivity", 0f);
-            m_vaporator.UpgradeValues.Add("Effectiveness", 1f);
-            m_vaporator.UpgradeValues.Add("PowerEfficiency", 1f);
+            //m_vaporator.UpgradeValues.Add("Productivity", 0f);
+            //m_vaporator.UpgradeValues.Add("Effectiveness", 1f);
+            //m_vaporator.UpgradeValues.Add("PowerEfficiency", 1f);
 			
             builder = objectBuilder;
 
             // MUST set the object to update atleast ONCE because you can not modify the inventory inside Init method!!!
             Entity.NeedsUpdate = MyEntityUpdateEnum.EACH_100TH_FRAME;
-
+			
             terminalBlock = Entity as Sandbox.ModAPI.IMyTerminalBlock;
             terminalBlock.AppendingCustomInfo += appendCustomInfo;
         }
-
+		
         public override void UpdateBeforeSimulation100()
         {
             w_density = atmoDet.AtmosphereDetectionVaporator (this.Entity);
-            IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(0) as IMyInventory;
+            IMyInventory inventory = ((Sandbox.ModAPI.IMyTerminalBlock)Entity).GetInventory(1) as IMyInventory;
 			
 			//check to see if the block is on
 			if (m_vaporator.Enabled && m_vaporator.IsWorking && m_vaporator.IsFunctional)
-			{                        
-				m_vaporator.PowerConsumptionMultiplier =  10 * (1f + m_vaporator.UpgradeValues["Productivity"]) * (1f / m_vaporator.UpgradeValues["PowerEfficiency"]);
+			{    
+
+				try
+				{
+					var sink = Entity.Components.Get<MyResourceSinkComponent>();
+
+					poweruse = 0.4f * (1f + m_vaporator.UpgradeValues["Productivity"]) * (1f / m_vaporator.UpgradeValues["PowerEfficiency"]);
+					
+					if(sink != null)
+					{
+						sink.SetRequiredInputByType(MyResourceDistributorComponent.ElectricityId, poweruse);
+					}
+				}
+				catch(Exception e)
+				{
+					MyAPIGateway.Utilities.ShowNotification("[ Error in " + GetType().FullName + ": " + e.Message + " ]", 10000, MyFontEnum.Red);
+					MyLog.Default.WriteLine(e);
+				}		
+				//m_vaporator.PowerConsumptionMultiplier =  10 * (1f + m_vaporator.UpgradeValues["Productivity"]) * (1f / m_vaporator.UpgradeValues["PowerEfficiency"]);
 			
-				VRage.MyFixedPoint amount = (VRage.MyFixedPoint)(0.04 * (w_density * m_vaporator.UpgradeValues["Effectiveness"]) * (1 + m_vaporator.UpgradeValues["Productivity"]));
+				VRage.MyFixedPoint amount = (VRage.MyFixedPoint)(0.2 * (w_density * m_vaporator.UpgradeValues["Effectiveness"]) * (1 + m_vaporator.UpgradeValues["Productivity"]));
                 inventory.AddItems(amount, new MyObjectBuilder_Ore() { SubtypeName = "Ice" });
+			
 			}
-
 				
-
             terminalBlock.RefreshCustomInfo();
-
             base.UpdateBeforeSimulation100();
         }
 
@@ -84,20 +100,20 @@ namespace Slowpokefarm.Vaporator
         public void appendCustomInfo(Sandbox.ModAPI.IMyTerminalBlock block, StringBuilder info)
         {
             info.Clear ();
-			
-            info.Append("Current Required Input: ");
-            info.Append((m_vaporator.Enabled && m_vaporator.IsWorking && m_vaporator.IsFunctional) ? (100f * (1f + m_vaporator.UpgradeValues["Productivity"]) * (1f / m_vaporator.UpgradeValues["PowerEfficiency"])).ToString("N") : 0f.ToString("N"));
-            info.Append(" kW \n");
-            info.AppendFormat("\n\n");
-            info.Append("Productivity: ");
-            info.Append(((m_vaporator.UpgradeValues["Productivity"] + 1f) * 100f).ToString("F0"));
-            info.Append("%\n");
-            info.Append("Effectiveness: ");
-            info.Append(((m_vaporator.UpgradeValues["Effectiveness"]) * 100f).ToString("F0"));
-            info.Append("%\n");
-            info.Append("Power Efficiency: ");
-            info.Append(((m_vaporator.UpgradeValues["PowerEfficiency"]) * 100f).ToString("F0"));
-            info.Append("%\n");
+            //info.Append("Current Required Input: ");
+            //info.Append((m_vaporator.Enabled && m_vaporator.IsWorking && m_vaporator.IsFunctional) ? (100f * (1f + m_vaporator.UpgradeValues["Productivity"]) * (1f / m_vaporator.UpgradeValues["PowerEfficiency"])).ToString("N") : 0f.ToString("N"));
+            //info.Append(" kW \n");
+            info.AppendFormat("\n");
+            //info.AppendFormat("\n");
+            //info.Append("Productivity: ");
+            //info.Append(((m_vaporator.UpgradeValues["Productivity"] + 1f) * 100f).ToString("F0"));
+            //info.Append("%\n");
+            //info.Append("Effectiveness: ");
+            //info.Append(((m_vaporator.UpgradeValues["Effectiveness"]) * 100f).ToString("F0"));
+            //info.Append("%\n");
+            //info.Append("Power Efficiency: ");
+            //info.Append(((m_vaporator.UpgradeValues["PowerEfficiency"]) * 100f).ToString("F0"));
+            //info.Append("%\n");
 			
             info.AppendLine ("Atmosphere density: " + w_density.ToString("N") + " p");
             // info.AppendLine ("OwnerId: " + ((MyCubeBlock)Entity).OwnerId );
